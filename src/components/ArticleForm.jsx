c2f4aa256968f903e5762810d51019f57cb1660c
Jane Doe <alsousha@mail.ru>
2025-09-16 21:28:21 +0300

    edit

--- src/components/ArticleForm.jsx
+++ src/components/ArticleForm.jsx
@@ -1,70 +1,51 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function ArticleForm({ article, onSave, onCancel }) {
-  const [title, setTitle] = useState(article.title);
-  const [author, setAuthor] = useState(article.author);
-  const [createDate, setCreateDate] = useState(article.createDate);
-  const [description, setDescription] = useState(article.description);
-  const [img, setImg] = useState(article.img || '');
+  const [formData, setFormData] = useState({
+    title: article.title,
+    img: article.img,
+    author: article.author,
+    createDate: article.createDate,
+    description: article.description,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //  const updatedArticle = {
-    //    ...article,
-    //    title,
-    //    author,
-    //    createDate,
-    //    description,
-    //  };
-    const updatedArticle = {
-      ...article,
-      title,
-      author,
-      createDate,
-      description,
-      img, // добавили сюда
-    };
-    onSave(updatedArticle);
+    onSave(formData); // передаём наружу
   };
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImg(reader.result); // Base64-строка
-      };
-      reader.readAsDataURL(file);
-    }
-  };
+
   return (
-    <form onSubmit={handleSubmit} className='article-form'>
-      <h3>Edit Article</h3>
+    <form onSubmit={handleSubmit}>
+      <h3>Edit form</h3>
       <div>
         <label>Title:</label>
-        <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type='text' name='title' value={formData.title} onChange={handleChange} />
       </div>
-
       <div>
         <label>Author:</label>
-        <input type='text' value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <input type='text' name='author' value={formData.author} onChange={handleChange} />
       </div>
-
       <div>
         <label>Date:</label>
-        <input type='date' value={createDate} onChange={(e) => setCreateDate(e.target.value)} />
+        <input type='date' name='createDate' value={formData.createDate} onChange={handleChange} />
       </div>
-
       <div>
         <label>Description:</label>
-        <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+        <input
+          type='text'
+          name='description'
+          value={formData.description}
+          onChange={handleChange}
+        />
       </div>
-      <div>
-        <label>Upload Image:</label>
-        <input type='file' accept='image/*' onChange={(e) => handleImageChange(e)} />
-      </div>
-
-      {img && <img src={img} alt='Preview' width='150' />}
-
       <button type='submit'>Save</button>
       <button type='button' onClick={onCancel}>
         Cancel
